504de13454a29feec9e2aa73b3dbd5d55ed56de9
kandeldeepak <kandeldeepak46@gmail.com>
2021-03-20 15:37:37 +0545

    code refactored with black

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='its wafer project using mlops',
-    author='kandel deepak',
-    license='MIT',
+    version="0.1.0",
+    description="its wafer project using mlops",
+    author="kandel deepak",
+    license="MIT",
 )
--- src/pipeline_01_data_preperation.py
+++ src/pipeline_01_data_preperation.py
@@ -1,4 +1,4 @@
-import os 
+import os
 import argparse
 import yaml
 import logging
@@ -14,12 +14,12 @@ def main(config_path, datasource):
     config = read_params(config_path)
     print(config)
 
-if __name__ == '__main__':
-    args = argparse.ArgumentParser()
-    default_config_path = os.path.join("config","params.yaml")
-    args.add_argument('--config', default=default_config_path)
-    args.add_argument('--datasource', default=None)
-    
 
-    parsed_args  = args.parse_args()
-    main(config_path = parsed_args.config, datasource = parsed_args.datasource)
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    default_config_path = os.path.join("config", "params.yaml")
+    args.add_argument("--config", default=default_config_path)
+    args.add_argument("--datasource", default=None)
+
+    parsed_args = args.parse_args()
+    main(config_path=parsed_args.config, datasource=parsed_args.datasource)
